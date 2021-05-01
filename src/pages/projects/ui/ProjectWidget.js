19cd04455c52401c251230caa0b86c8fc4395fed
Abdul Wahhab Khan <>
2021-05-02 00:58:07 +0100

    project action button

--- src/pages/projects/ui/ProjectWidget.js
+++ src/pages/projects/ui/ProjectWidget.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {Col, OverlayTrigger, Popover, ProgressBar, Row} from "react-bootstrap";
+import React from 'react'
+import { Col, ProgressBar, Row, Dropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import Moment from 'react-moment';
 import Avatar from "react-avatar";
@@ -7,12 +7,12 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faCheck, faEllipsisV, faPencilAlt, faTimes} from "@fortawesome/free-solid-svg-icons";
 import settings from "../../../config/settings";
 
+
+
 const ProjectWidget =React.memo((props) => {
-        const [show, setShow] = useState(false);
+
         const status = new Date(props.project.end_date) > new Date() ? 'green' : 'orange'
-        const handleClick = (event) => {
-            setShow(!show);
-        };
+
         return (
             <>
                 <div className={'panel ' + status}>
@@ -59,46 +59,44 @@ const ProjectWidget =React.memo((props) => {
                                         <div className={'project-label'}>
                                             &nbsp;
                                         </div>
+                                        <Dropdown drop={'left'} className={'dropdown-actions'} >
+                                            <Dropdown.Toggle
 
-                                        <OverlayTrigger
-                                            //trigger={['focus', 'click']}
-                                            placement="left"
-                                            show={show}
-                                            delay={200}
-                                            overlay={
-                                                <Popover id={props.project.id}>
-                                                    <Popover.Content>
-                                                        <ul className="popover-actions">
-                                                            <li onClick={() => {
-                                                                handleClick();
-                                                                props.onDelete(props.project.id);
-                                                            }}>
-                                                                <FontAwesomeIcon icon={faTimes} size={'2x'}/>
-                                                                Delete
-                                                            </li>
-                                                            <li onClick={() => {
-                                                                handleClick();
-                                                                props.onEdit(props.project)
-                                                            }}>
-                                                                <FontAwesomeIcon icon={faPencilAlt} size={'2x'}/>
-                                                                Edit
-                                                            </li>
-                                                            <li onClick={() => {
-                                                                handleClick();
-                                                                props.onComplete(props.project.id)
-                                                            }}>
-                                                                <FontAwesomeIcon icon={faCheck} size={'2x'}/>
-                                                                Complete
-                                                            </li>
-                                                        </ul>
-                                                    </Popover.Content>
-                                                </Popover>
-                                            }
-                                        >
-                                            <button className={'btn btn-sm btn-circle btn-grey'} onClick={handleClick}>
-                                                <FontAwesomeIcon icon={faEllipsisV}/>
-                                            </button>
-                                        </OverlayTrigger>
+                                                className={'btn-circle'}
+                                                id={`action_${props.project.id}`}
+                                                variant={'grey'}
+                                                size={'sm'}
+                                            >
+                                                <FontAwesomeIcon  icon={faEllipsisV}/>
+                                            </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item
+                                                    onClick={() => {
+                                                        props.onEdit(props.project.id);
+                                                    }}
+                                                >
+                                                    <FontAwesomeIcon className={'svg-icon'} icon={faPencilAlt} />
+                                                    Edit
+                                                </Dropdown.Item>
+                                                <Dropdown.Item
+                                                    onClick={() => {
+                                                        props.onDelete(props.project.id);
+                                                    }}
+                                                >
+                                                    <FontAwesomeIcon className={'svg-icon'} icon={faTimes} />
+                                                    Delete
+                                                </Dropdown.Item>
+                                                <Dropdown.Item
+                                                    onClick={() => {
+                                                        props.onComplete(props.project.id);
+                                                    }}
+                                                >
+                                                    <FontAwesomeIcon className={'svg-icon'} icon={faCheck} />
+                                                    Complete
+                                                </Dropdown.Item>
+
+                                            </Dropdown.Menu>
+                                        </Dropdown>{' '}
 
                                     </Col>
                                 </Row>
